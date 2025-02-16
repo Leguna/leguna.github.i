e12df8fcac4930cd96b77b3966ad0bc6bfca512d
Ahmad Tuflihun <ahmadtuflihunxd@gmail.com>
2025-02-16 16:37:15 +0700

    chore: update ESLint configuration to remove standard extension

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ["plugin:react/recommended", "standard"],
+  extends: ["plugin:react/recommended"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly",
